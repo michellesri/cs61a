ff0f4cf6b5a05d8c47bb3246d21ca4486d70abdf
Michelle Sri <michelle.sri@gmail.com>
2017-09-28 16:51:27 -0700

    finished select_dice function with all tests passing

--- Project1_Hog/hog/hog.py
+++ Project1_Hog/hog/hog.py
@@ -120,8 +120,10 @@ def select_dice(score, opponent_score, dice_swapped):
     DICE_SWAPPED is True if and only if four-sided dice are being used.
     """
     # BEGIN PROBLEM 4
-    "*** REPLACE THIS LINE ***"
-    dice = six_sided  # Replace this statement
+    if dice_swapped:
+        dice = four_sided
+    else:
+        dice = six_sided
     # END PROBLEM 4
     if (score + opponent_score) % 7 == 0:
         dice = reroll(dice)
