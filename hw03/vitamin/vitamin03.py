1c64dd1f6eebadc25ab1d37d049789976289494f
Michelle Sri <michelle.sri@gmail.com>
2017-10-13 16:45:45 -0700

    wrote accumulate and accumulate recursive in vitamins

--- hw03/vitamin/vitamin03.py
+++ hw03/vitamin/vitamin03.py
@@ -49,3 +49,38 @@ def summation(n, term):
         return term(n)
     if n > 1:
         return term(n) + summation(n - 1, term)
+
+def product(n, term):
+    if n == 1:
+        return term(n)
+    if n > 1:
+        return term(n) * product(n - 1, term)
+
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)   # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    total = base
+    k = n
+    while k > 0:
+        total = combiner(total, term(k))
+        k -= 1
+    return total
+
+def accumulate_recursive(combiner, base, n, term):
+    if n == 1:
+        return combiner(base, term(n))
+    return accumulate_recursive(combiner, combiner(base, term(n)), n - 1, term)
