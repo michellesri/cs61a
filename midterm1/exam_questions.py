70197f2ba1182513fc99e7745bb1fa2db562abcd
Michelle Sri <michelle.sri@gmail.com>
2017-10-21 12:04:39 -0700

    finished can_win function

--- midterm1/exam_questions.py
+++ midterm1/exam_questions.py
@@ -97,6 +97,23 @@ def total_ones(n):
 
     return one_count + total_ones(n - 1)
 
+def can_win(number):
+    """Returns True if the current player is guaranteed a win
+    starting from the given state. It is impossible to win a game
+    from an invalid game state.
+
+    >>> can_win (-1) # invalid game state
+    False
+    >>> can_win (3) # take all three !
+    True
+    >>> can_win (4)
+    False
+    """
+    if number > 0 and number <= 3:
+        return True
+    return False
+
+
 
 
 if __name__ == "__main__":
