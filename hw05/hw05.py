cc8e00b15f0f818bb997ef0f9d4c02918329797a
Michelle Sri <michelle.sri@gmail.com>
2017-11-02 16:30:19 -0700

    unlocked tests for div_interval and finished function with all tests passing

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -203,6 +203,7 @@ def div_interval(x, y):
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert lower_bound(y) > 0 and upper_bound(y) > 0
     reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
