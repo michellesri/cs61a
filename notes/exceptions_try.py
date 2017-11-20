93b4bca96004f48861f8d9b1f1dd0afcfcb9d601
Michelle Sri <michelle.sri@gmail.com>
2017-11-19 16:14:28 -0800

    defined invert_safe function which handles zero division error

--- notes/exceptions_try.py
+++ notes/exceptions_try.py
@@ -2,3 +2,10 @@ def invert(x):
     y = 1/x
     print('Never printed if x is 0')
     return y
+
+def invert_safe(x):
+    try:
+        return invert(x)
+    except ZeroDivisionError as e:
+        print('handled', e)
+        return 0
