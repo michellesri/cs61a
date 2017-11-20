7a2d0096f61f1ce23bec1485e091cf72d6510c78
Michelle Sri <michelle.sri@gmail.com>
2017-11-19 16:17:34 -0800

    added edge case example

--- notes/exceptions_try.py
+++ notes/exceptions_try.py
@@ -9,3 +9,6 @@ def invert_safe(x):
     except ZeroDivisionError as e:
         print('handled', e)
         return 0
+
+# invert_safe(1/0) this try statement is never executed
+# before we call the function, we raise the exception
