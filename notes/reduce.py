c94ea2e33204854b45d2e5a87d147c2415036158
Michelle Sri <michelle.sri@gmail.com>
2017-11-20 16:38:46 -0800

    wrote reduce_recursive. doctests passing

--- notes/reduce.py
+++ notes/reduce.py
@@ -13,3 +13,19 @@ def reduce(f, s, initial):
     for x in s:
         initial = f(initial, x)
     return initial
+
+
+def reduce_recursive(f, s, initial):
+    """combine elements of s using f starting at initial_balance
+
+    >>> reduce(mul, [2, 4, 8], 1)
+    64
+
+    >>> reduce(add, [1, 2, 3, 4], 0)
+    10
+    """
+    if not s:
+        return initial
+    else:
+        first, rest = s[0], s[1:]
+        return reduce_recursive(f, rest, f(initial, first))
