fd076ff63724c8b4efca4b6f377c46989dcc2b86
Michelle Sri <michelle.sri@gmail.com>
2017-11-22 13:25:33 -0800

    added doctests for scheme_read in scheme.py

--- notes/scheme.py
+++ notes/scheme.py
@@ -1,4 +1,11 @@
 def scheme_read(src):
+    """Read the next expression from src, a Buffer of tokens.
+
+    >>> lines = ['(+ 1', '(+ 23 4)) (']
+    >>> src = Buffer(tokenize_lines(lines))
+    >>> print(scheme_read(src))
+    (+ 1 (+ 23 4))
+    """
 
 
 def read_tail(src):
