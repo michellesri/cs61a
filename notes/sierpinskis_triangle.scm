56787b142d77ea24e71cfbc20e9c16b530183d04
Michelle Sri <michelle.sri@gmail.com>
2017-11-21 17:04:59 -0800

    added tri fn. repeats fn 3 times each followed by a 120 degree turn

--- notes/sierpinskis_triangle.scm
+++ notes/sierpinskis_triangle.scm
@@ -6,6 +6,10 @@
     (begin (fn) (repeat (- k 1) fn))
     (fn)))
 
+(define (tri fn)
+  ; Repeat fn 3 times, each followed by a 120 degree turn.
+  (repeat 3 (lambda () (fn) (lt 120))))
+
 (define (sier d j)
   ; Draw three legs of Sierpinski's triangle to depth d.
   (tri (lambda ()
