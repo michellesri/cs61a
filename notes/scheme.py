fcd1c9b1ce18cce46bcc8ff664cd115f1a9074d4
Michelle Sri <michelle.sri@gmail.com>
2017-11-22 13:32:22 -0800

    added more doctests to read_tail

--- notes/scheme.py
+++ notes/scheme.py
@@ -11,4 +11,12 @@ def scheme_read(src):
 def read_tail(src):
     """Return the remainder of a list in src, starting
     before an element or ).
+
+    >>> read_tail(Buffer(tokenize_lines([')'])))
+    nil
+    >>> read_tail(Buffer(tokenize_lines(['2 3)'])))
+    Pair(2, Pair(3, nil))
+
+    >>> read_tail(Buffer(tokenize_lines(['2 (3 4))'])))
+    Pair(2, Pair(Pair(3, Pair(4, nil)), nil))
     """
