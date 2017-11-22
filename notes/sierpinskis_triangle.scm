fd15b1e01494dfec6d208c19b3f460d5df5b7c7a
Michelle Sri <michelle.sri@gmail.com>
2017-11-21 17:03:16 -0800

    added sier fn which draws 3 legs of the triangle

--- notes/sierpinskis_triangle.scm
+++ notes/sierpinskis_triangle.scm
@@ -5,3 +5,9 @@
   (if (> k 1)
     (begin (fn) (repeat (- k 1) fn))
     (fn)))
+
+(define (sier d j)
+  ; Draw three legs of Sierpinski's triangle to depth d.
+  (tri (lambda ()
+    (if ((= k 1)(fd d) (leg d k)))))
+)
