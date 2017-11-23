b055bf419532370df183cf032b96f577cc868ab1
Michelle Sri <michelle.sri@gmail.com>
2017-11-23 14:49:28 -0800

    finished calc_eval function

--- notes/calculator.py
+++ notes/calculator.py
@@ -6,3 +6,10 @@ def calc_eval(exp):
     >>> calc_eval(as_scheme_list('+', 2, as_scheme_list('/', 40, 5)))
     10
     """
+    if type(exp) in (int, float):
+        return simplify(exp)
+    elif isinstance(exp, Pair):
+        arguments = exp.second.map(calc_eval)
+        return simplify(calc_apply(exp.first, arguments))
+    else:
+        raise TypeError(str(exp) + ' is not a number or call expression')
