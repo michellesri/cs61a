2beb02a97fd60b0c2e45838e6d000a16d2db80ac
Michelle Sri <michelle.sri@gmail.com>
2017-12-21 12:53:35 -0800

    finished wwpd front page of mt1, but need to go back to el(3, el) problem

--- midterm1/mt1Fall2017.py
+++ midterm1/mt1Fall2017.py
@@ -1,21 +1,21 @@
-# aaron, burr = 2, 5
-# aaron, burr = 4, aaron + 1
-# 
-# hamil = 10
-# 
-# def alex(hamil):
-#     def g(w):
-#         hamil = 2 * w
-#         print('blah: ' + str(hamil))
-#         print(hamil, w)
-#         w = hamil
-#         return hamil
-#     w = 5
-#     alex = g(w + 1)
-#     print(w, alex, hamil)
-# 
-# print(aaron, burr)
-# alex(3)
+aaron, burr = 2, 5
+aaron, burr = 4, aaron + 1
+
+hamil = 10
+
+def alex(hamil):
+    def g(w):
+        hamil = 2 * w
+        print('blah: ' + str(hamil))
+        print(hamil, w)
+        w = hamil
+        return hamil
+    w = 5
+    alex = g(w + 1)
+    print(w, alex, hamil)
+
+print(aaron, burr)
+alex(3)
 
 def el(i, za):
     def angelica():
@@ -37,3 +37,5 @@ K(3)(2) # lambda
 def pr(x):
     print(x)
     return x
+
+pr(True) and pr(0) and pr(1)
