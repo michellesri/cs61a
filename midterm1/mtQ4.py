94f343ce0673338677e5373835cc81b09b3b9eb1
Michelle Sri <michelle.sri@gmail.com>
2017-12-22 13:46:26 -0800

    defined function and added doc tests

--- midterm1/mtQ4.py
+++ midterm1/mtQ4.py
@@ -1,2 +1,17 @@
 # Implement collapse, which takes a non-negative integer, and returns the result of removing all digits
 # from it that duplicate the digit immediately to their right.
+
+def collapse(n):
+    """For non-negative N, the result of removing all digits that are equal
+    to the digit on their right, so that no adjacent digits are the same.
+    >>> collapse(1234)
+    1234
+    >>> collapse(12234441)
+    12341
+    >>> collapse(0)
+    0
+    >>> collapse(3)
+    3
+    >>> collapse(11200000013333)
+    12013
+    """
