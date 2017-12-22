8d74b73e178cc8bc3e106145f6ad4bf1c29537c3
Michelle Sri <michelle.sri@gmail.com>
2017-12-22 13:29:32 -0800

    started mtQ3

---
+++ midterm1/mtQ3.py
@@ -0,0 +1,4 @@
+# Triangulate
+# It’s easy to see that in any triangle, each side must be shorter than the sum of the other two. Implement
+# triangle, which takes three positive numbers, a, b, and c, and returns whether these three numbers could
+# possibly be the lengths of the three sides of a triangle.
