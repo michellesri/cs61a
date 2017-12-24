12a54db0fc564d8e23cf70f3f5d81fd2b36edbea
Michelle Sri <michelle.sri@gmail.com>
2017-12-24 12:25:45 -0800

    finished defining __init__ for class Plant

--- midterm2/mtQ1.py
+++ midterm2/mtQ1.py
@@ -5,3 +5,6 @@ class Plant:
     def __init__(self):
         self.k = Plant.k
         Plant.k = self.k + 1
+        if self.k > 3:
+            Plant.name = lambda t: 'tree'
+            Plant.k = 6
