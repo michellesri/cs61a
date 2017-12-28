a7105aa920cd3ffe30d09a4cc49124417621dc09
Michelle Sri <michelle.sri@gmail.com>
2017-12-28 13:04:12 -0800

    started Q1 for mt2 fall 2015

---
+++ midterm2Fall2015/Q1.py
@@ -0,0 +1,5 @@
+# For each of the expressions in the table below, write the output displayed by the interactive Python interpreter
+# when the expression is evaluated. The output may have multiple lines. If more than 3 lines are displayed, just
+# write the first 3. If an error occurs, write “Error”. If evaulation would run forever, write “Forever”.
+# The first two rows have been provided as examples.
+# Assume that you have started python3 and executed the following statements (which do not cause errors):
