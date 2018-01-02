482436c3a695243a2ddda4c108e81ecf57175be6
Michelle Sri <michelle.sri@gmail.com>
2018-01-02 11:15:40 -0800

    finished hw09 Q5: no dots

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -28,7 +28,12 @@
 )
 
 (define (nodots s)
-  'YOUR-CODE-HERE
+  (cond ((null? s) s)
+    ((and (pair? s) (not (or (pair? (cdr s)) (null? (cdr s)))))
+      (list (nodots (car s)) (cdr s)))
+    ((pair? s) (cons (nodots (car s)) (nodots (cdr s))))
+    (else s)
+    )
 )
 
 ; Sets as sorted lists
