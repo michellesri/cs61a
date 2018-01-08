19b4284cb261b03715ba4b3ad6f43eec228cbf09
Michelle Sri <michelle.sri@gmail.com>
2018-01-08 14:39:12 -0800

    finished hw10 Q1: substitute

--- hw10/problems/hw10.scm
+++ hw10/problems/hw10.scm
@@ -1,5 +1,10 @@
 (define (substitute s old new)
-  'YOUR-CODE-HERE
+  (cond ((null? s) nil)
+    ((pair? (car s)) (cons (substitute (car s) old new)
+                      (substitute (cdr s) old new)))
+    ((equal? (car s) old) (cons new
+                            (substitute (cdr s) old new)))
+      (else (cons (car s) (substitute (cdr s) old new))))
 )
 
 
